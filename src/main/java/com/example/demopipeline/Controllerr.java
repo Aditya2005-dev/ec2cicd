dfd7c9103eb026dee8ed6ed8ca9e0d31d5b357c4
Aditya kulkarni <kulkarniadi09@gmail.com>
2026-08-04 23:49:22 +0530

    Check the page

--- src/main/java/com/example/demopipeline/Controllerr.java
+++ src/main/java/com/example/demopipeline/Controllerr.java
@@ -1,12 +1,13 @@
 package com.example.demopipeline;
 
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-@RestController
-public class Controller {
+@Controller
+public class Controllerr {
 
     
     @GetMapping("/")
